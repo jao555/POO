0a5cc302ca61fdc7354cbe6ac10b6fb66feb2e6a
ALEJOLIZ <ajejo.lizcano@gmail.com>
2026-03-14 01:30:39 -0500

    Mammal v1

---
+++ src/Mammal.java
@@ -0,0 +1,15 @@
+public class Mammal extends Animal {
+    public Mammal(String id, double weight, int age, String species) {
+        super(id, weight, age, species);
+    }
+
+    @Override
+    public void count() {
+
+    }
+
+    @Override
+    public void calculateFoodRation() {
+
+    }
+}
